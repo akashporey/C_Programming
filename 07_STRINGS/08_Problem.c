fb7843780d4b9aaedcb9e87d9d056f87eb295274
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-06 01:26:59 +0530

    Add New File

---
+++ 07_STRINGS/08_Problem.c
@@ -0,0 +1,30 @@
+/* Write a program to check whether a given character is present in a string or not.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    int contains=0;
+    char st[] = "I am doing B.tech";
+    for (int i = 0; i < strlen(st); i++)
+    {
+        if(st[i]==ch){
+            contains=1;
+            break;
+        }
+    }
+    
+    if (contains==1)
+    {
+        printf("Yes, this character is present.");
+    }
+    else{
+        printf("No, this character not present.");
+    }
+    
+    return 0;
+}
