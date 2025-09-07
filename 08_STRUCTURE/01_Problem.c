c0a869ada9814d8c2cfd0367b2b2ef9d61f852a9
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-07 16:12:11 +0530

    Add new file

---
+++ 08_STRUCTURE/01_Problem.c
@@ -0,0 +1,17 @@
+/* Create a two-dimensional vector using structures in C.
+*/
+
+#include <stdio.h>
+
+struct vector
+{
+    int i;
+    int j;
+};
+
+int main() {
+    struct vector v={1,2};
+
+    printf("The values of 2D vector is %di + %dj", v.i, v.j);
+    return 0;
+}
