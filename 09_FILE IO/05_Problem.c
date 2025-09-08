80cf59d817f7c3431144e22dd9eca37090ad9b5a
Akash Porey <akashporeyofficial2507@gmail.com>
2025-09-09 00:28:22 +0530

    Add New File

---
+++ 09_FILE IO/05_Problem.c
@@ -0,0 +1,18 @@
+// Write a program to modify a file containing an integer to double its value.
+
+#include <stdio.h>
+
+int main() {
+    FILE* ptr;
+    int num;
+    ptr= fopen("05file.txt", "r");
+    fscanf(ptr, "%d", &num);
+    fclose(ptr);
+    
+    ptr= fopen("05file.txt", "w");
+    fprintf(ptr, "%d", 2*num);
+
+    fclose(ptr);
+
+    return 0;
+}
